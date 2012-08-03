7f4c061934ddae15a68398696b0378b7d857f6d3
Lito <lito@eordes.com>
2012-08-03 14:22:29 +0200

    Hide pending list games when no pending games are available

--- themes/default/index.php
+++ themes/default/index.php
@@ -2,13 +2,19 @@
 defined('BASE_PATH') or die();
 
 foreach (array('pending', 'turn', 'waiting', 'ended') as $status) {
+    $games = $Api->getGames($status);
+
+    if (!$games && ($status === 'pending')) {
+        continue;
+    }
+
     echo '<div class="page-header">';
     echo '<h1>'.__(ucfirst($status)).'</h1>';
     echo '</div>';
 
-    if ($games = $Api->getGames($status)) {
+    if ($games) {    
         include ($Theme->get('sub-games-list.php'));
-    } else {
+    } else if ($status !== 'pending') {
         echo '<div class="well">'.__('No games here :)').'</div>';
     }
 }
