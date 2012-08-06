8f19ba35cee49f76cc190f5865c53bea401dc7f6
Lito <lito@eordes.com>
2012-08-06 16:09:02 +0200

    Added contact line in about section

--- themes/default/about.php
+++ themes/default/about.php
@@ -9,4 +9,7 @@
     <p>It's easy to install and requires PHP 5.3.</p>
     <p>Only login using user and password is available (no facebook or twitter).</p>
     <p>You can get the sorce code at <a href="https://github.com/eusonlito/Apalabro">Github</a>.</p>
+    <?php if ($Api->logged()) { ?>
+    <p>Also, if you have some suggestion or doubt, you can contact with me at <a href="mailto:lito@eordes.com">lito@eordes.com</a>.</p>
+    <?php } ?>
 </div>
