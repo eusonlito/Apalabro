b4f76d22572e975d92d9f68382c6325148fc5533
Lito <lito@eordes.com>
2012-08-09 13:14:53 +0200

    Updated styles

--- themes/default/index.php
+++ themes/default/index.php
@@ -1,20 +1,23 @@
-<?php
-defined('BASE_PATH') or die();
+<?php defined('BASE_PATH') or die(); ?>
 
-foreach (array('turn', 'waiting', 'pending', 'ended') as $status) {
-    $games = $Api->getGames($status);
+<div class="home">
+    <?php
+    foreach (array('turn', 'waiting', 'pending', 'ended') as $status) {
+        $games = $Api->getGames($status);
 
-    if (!$games && ($status === 'pending')) {
-        continue;
+        if (!$games && ($status === 'pending')) {
+            continue;
+        }
+
+        echo '<div class="page-header">';
+        echo '<h1>'.__(ucfirst($status)).'</h1>';
+        echo '</div>';
+
+        if ($games) {    
+            include ($Theme->get('sub-games-list.php'));
+        } else if ($status !== 'pending') {
+            echo '<div class="well height-auto">'.__('No games here :)').'</div>';
+        }
     }
-
-    echo '<div class="page-header">';
-    echo '<h1>'.__(ucfirst($status)).'</h1>';
-    echo '</div>';
-
-    if ($games) {    
-        include ($Theme->get('sub-games-list.php'));
-    } else if ($status !== 'pending') {
-        echo '<div class="well">'.__('No games here :)').'</div>';
-    }
-}
+    ?>
+</div>
