3675bfe9a328e197f259b0341fdf36c7da1c741a
Lito <lito@eordes.com>
2012-08-14 14:56:56 +0200

    Updated languages and added game available languages to about section

--- aux/dynamic-strings.php
+++ aux/dynamic-strings.php
@@ -1,10 +1,14 @@
 <?php
 defined('BASE_PATH') or die();
 
+__('language_de');
 __('language_en');
 __('language_en-uk');
 __('language_es');
+__('language_fr');
 __('language_gl');
+__('language_it');
+__('language_pt');
 
 __('Turn');
 __('Waiting');
--- themes/default/about.php
+++ themes/default/about.php
@@ -1,15 +1,30 @@
 <?php defined('BASE_PATH') or die(); ?>
 
-<div class="page-header">
-    <h1><?php __e('About Apalabro!'); ?></h1>
-</div>
+<section>
+    <div class="page-header">
+        <h1><?php __e('About Apalabro!'); ?></h1>
+    </div>
 
-<div class="lead">
-    <p><strong>Apalabro!</strong> is a PHP project developed by <a href="https://github.com/eusonlito">Lito</a> to create an Apalabrados (Angry Words) web interface.</p>
-    <p>It's easy to install and requires PHP 5.3.</p>
-    <p>Only login using user and password is available (Facebook ins't implemented yet).</p>
-    <p>You can get the sorce code at <a href="https://github.com/eusonlito/Apalabro">Github</a>.</p>
-    <?php if ($Api->logged()) { ?>
-    <p>Also, if you have some suggestion or doubt, you can contact with me at <a href="mailto:lito@eordes.com">lito@eordes.com</a>.</p>
-    <?php } ?>
-</div>
+    <div class="lead">
+        <p><strong>Apalabro!</strong> is a PHP project developed by <a href="https://github.com/eusonlito">Lito</a> to create an Apalabrados (Angry Words) web interface.</p>
+        <p>It's easy to install and requires PHP 5.3.</p>
+        <p>Only login using user and password is available (Facebook ins't implemented yet).</p>
+        <p>You can get the sorce code at <a href="https://github.com/eusonlito/Apalabro">Github</a>.</p>
+
+        <?php if ($Api->logged()) { ?>
+        <p>Also, if you have some suggestion or doubt, you can contact with me at <a href="mailto:lito@eordes.com">lito@eordes.com</a>.</p>
+        <?php } ?>
+    </div>
+</section>
+
+<section>
+    <div class="page-header">
+        <h1><?php __e('Available Languages'); ?></h1>
+    </div>
+
+    <div class="row">
+        <?php foreach ($Api->getLanguages() as $language) { ?>
+        <img src="<?php echo BASE_WWW.'languages/'.$language; ?>/flag.png" alt="<?php __e('language_'.$language); ?>" title="<?php __e('language_'.$language); ?>" class="span1" />
+        <?php } ?>
+    </div>
+</section>
